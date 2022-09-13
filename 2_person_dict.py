e755563370c4bc8ff8f8726b85417a9fcb451774
Ryan <rm@test.com>
2022-09-13 12:36:03 -0500

    changes

---
+++ 2_person_dict.py
@@ -0,0 +1,7 @@
+person = {}
+person["fname"] = "Joe"
+person["lname"] = "Fonebone"
+person["age"] = 51
+person["spouse"] = "Edna"
+person["children"] = ["Ralph", "Betty", "Joey"]
+person["pets"] = {"dog": "Fido", "cat": "Sox"}
